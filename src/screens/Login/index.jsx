ea53532ffabe3800fd34e7f02654dbb7614d7c10
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-04-22 21:52:20 -0300

    Add react-native-vector-icons package and update login screen

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Image } from 'react-native';
-
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import Title from '../../components/Title';
 
 import styles from './styles';
@@ -8,8 +8,24 @@ export default function Login() {
   return (
     <View style={styles.containerLogin}>
       <View style={styles.container}>
-        <Image style={{ width: 90, height: 60 }} source={require('../../../assets/images/newpack-logo.png')} />
-        <Text style={styles.title}>Bem Vindo!</Text>
+        <Image style={{ width: 130, height: 80 }} source={require('../../../assets/images/newpack-logo.png')} />
+        <Text style={styles.title}>Entre em seu perfil</Text>
+      </View>
+      <View style={styles.containerInps}>
+
+        <View style={styles.containerInps2}>
+          <View style={styles.inputContainer}>
+            <Icon color={"#b7b7b7"} name='user-check' size={18} />
+            <TextInput placeholder='Insira seu nome' style={styles.inputTxt} />
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon color={"#b7b7b7"} name='key' size={18} />
+            <TextInput placeholder='Insira sua senha' style={styles.inputTxt} />
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonTxt}>Entrar</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -6,15 +6,45 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#e8e8e8',
+    gap: 40,
   },
   container: {
     alignItems: 'center',
-    gap: 5,
+    gap: 12,
+
   },
   title: {
-    fontSize: 25,
+    fontSize: 30,
     fontWeight: 'bold',
     color: '#2f2f2f',
+  },
+  containerInps: {
+    width: '85%',
+  },
+  containerInps2: {
+    gap: 40,
+    marginBottom: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    padding: 17,
+    gap: 20,
+  },
+  inputTxt: {
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: '#4B6584',
+    padding: 17,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  buttonTxt: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
 
