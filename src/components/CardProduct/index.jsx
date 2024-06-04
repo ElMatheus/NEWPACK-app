21171fc291ed6e5ecaab61cbbe737385adc32133
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-06-03 22:28:29 -0300

    Update EXPO_PUBLIC_API_URL and add image support to CardProduct component

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import styles from './styles';
 
-const CardProduct = ({ name, unitary_price, toughness, dimension, cod }) => {
+const CardProduct = ({ name, image, unitary_price, toughness, dimension, cod }) => {
   return (
-    <View>
-      <Text style={styles.title}>{name}</Text>
+    <View style={styles.card}>
+      <Image source={{ uri: image }} style={styles.image} />
+      <View style={styles.containerCard}>
+        <View style={styles.containerCod}>
+          <Text style={styles.cod}>#{cod}</Text>
+        </View>
+        <Text>Teste textehih</Text>
+        {/* <View>
+
+        </View> */}
+      </View>
     </View>
   )
 }
--- src/components/CardProduct/styles.js
+++ src/components/CardProduct/styles.js
@@ -1,9 +1,30 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
+  card: {
+    backgroundColor: '#F3F3F3',
+    paddingHorizontal: 12,
+    paddingVertical: 16,
+    borderRadius: 8,
+    flexDirection: 'row',
+  },
+  containerCard: {
+    flex: 1,
+  },
+  containerCod: {
+    backgroundColor: '#4B6584',
+    padding: 4,
+    borderRadius: 4,
+    alignSelf: 'flex-end',
+  },
+  cod: {
+    color: '#17202b',
+    fontFamily: 'Poppins_500Medium',
+  },
+  image: {
+    width: 106,
+    height: 106,
+    borderRadius: 7.64219,
   },
 });
 
