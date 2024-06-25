5ef12111b9586d2456b3d0824839a32b555886a2
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-06-25 17:33:28 -0300

    chore: update ShoppingCart screen layout and styles

--- src/screens/ShoppingCart/index.jsx
+++ src/screens/ShoppingCart/index.jsx
@@ -1,13 +1,40 @@
-import { View, Text } from 'react-native';
-
-import Title from '../../components/Title';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default function ShoppingCart() {
   return (
     <View style={styles.container}>
-      <Title txt={'Carrinho de compras - 😁😁'} />
+      <View style={styles.leftPanel}>
+        <Text style={styles.title}>Produtos Selecionados</Text>
+        <View style={styles.productList}>
+          {/* Produtos serão adicionados aqui  */}
+        </View>
+      </View>
+      <View style={styles.rightPanel}>
+        <Text style={styles.title}>
+          Resumo do Carrinho
+        </Text>
+        <View style={styles.assistantView}>
+          <Text style={styles.txt}>
+            Quantidade de Produtos:
+          </Text>
+          <Text style={styles.txt}>
+            0
+          </Text>
+        </View>
+        <View style={styles.assistantView}>
+          <Text style={styles.txt}>
+            Preço Total:
+          </Text>
+          <Text style={styles.txt}>
+            R$ 0,00
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.txtBtn}>Concluir Compra</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/ShoppingCart/styles.js
+++ src/screens/ShoppingCart/styles.js
@@ -3,9 +3,44 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#f3f3f3',
+    margin: 20,
+  },
+  productList: {
+    backgroundColor: '#fff',
+    minHeight: 300,
+    borderColor: '#b9c3cd',
+    borderWidth: 1,
+    padding: 10,
+    borderStyle: 'dashed',
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 20,
+    color: '#000',
+    fontFamily: 'Poppins_500Medium',
+  },
+  assistantView: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    gap: 6,
+  },
+  txt: {
+    fontSize: 15,
+    color: '#000',
+    fontFamily: 'Poppins_400Regular',
+  },
+  btn: {
+    backgroundColor: '#4B6584',
+    padding: 10,
     alignItems: 'center',
-  }
+    borderRadius: 5,
+  },
+  txtBtn: {
+    fontSize: 16,
+    color: '#fff',
+    fontFamily: 'Poppins_500Medium',
+  },
 });
 
 export default styles;
