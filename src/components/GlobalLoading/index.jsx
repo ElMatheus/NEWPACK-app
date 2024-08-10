6b704e5cf6e220e690290a0e969065f11c095a5e
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-08-10 13:43:04 -0300

    redesign: loading

--- src/components/GlobalLoading/index.jsx
+++ src/components/GlobalLoading/index.jsx
@@ -1,12 +1,26 @@
-import { View, ActivityIndicator } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, ActivityIndicator, Text } from 'react-native';
 import styles from './styles.js';
 
 const GlobalLoading = () => {
+  const [dots, setDots] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => (prevDots.length < 3 ? prevDots + '.' : ''));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#4B6584" />
+      <View style={styles.indicatorContainer}>
+        <ActivityIndicator size="small" color="#4B6584" />
+      </View>
+      <Text style={styles.txtLoading}>Carregando{dots}</Text>
     </View>
-  )
-}
+  );
+};
 
 export default GlobalLoading;
--- src/components/GlobalLoading/styles.js
+++ src/components/GlobalLoading/styles.js
@@ -4,7 +4,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    gap: 20,
   },
+  txtLoading: {
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: 'Poppins_500Medium',
+    color: "#4B6584",
+  },
+  indicatorContainer: {
+    transform: [{ scale: 2 }],
+  }
 });
 
 export default styles;
