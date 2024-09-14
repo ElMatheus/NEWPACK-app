8368b47d66a401dc02d87b10fa4a9de883d51a89
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-09-14 12:50:31 -0300

    Refactor CardAddress component and styles

--- src/components/CardAddress/index.jsx
+++ src/components/CardAddress/index.jsx
@@ -1,27 +1,93 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import styles from './styles';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 const CardAddress = ({ address }) => {
+  const [moreInfo, setMoreInfo] = useState(false);
   return (
-    <View style={styles.containerAddress}>
-      <View style={styles.containerIcon}>
-        <MaterialCommunityIcons name="home-map-marker" size={42} color="#F3F3F3" />
+    <View style={styles.container}>
+      <View style={styles.containerAddress}>
+        <View style={styles.containerIcon}>
+          <MaterialCommunityIcons name="home-map-marker" size={42} color="#F3F3F3" />
+        </View>
+        <View style={styles.containerInfo}>
+          <Text style={styles.title}>{address.street}</Text>
+          <Text style={styles.txt}>{address.cep}</Text>
+          <Text style={styles.txt}>
+            {address.city}, {address.state} - {address.number}
+          </Text>
+          <Text style={styles.text}>{address.freight}</Text>
+        </View>
+        <View style={{ marginLeft: "auto" }}>
+          <TouchableOpacity onPress={() => setMoreInfo(!moreInfo)}>
+            <Entypo name="chevron-small-down" size={37} color="#4B6584" />
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.containerInfo}>
-        <Text style={styles.title}>{address.street}</Text>
-        <Text style={styles.txt}>{address.cep}</Text>
-        <Text style={styles.txt}>
-          {address.city}, {address.state} - {address.number}
-        </Text>
-        <Text style={styles.text}>{address.freight}</Text>
-      </View>
-      <View style={{ marginLeft: "auto" }}>
-        <TouchableOpacity>
-          <Entypo name="chevron-small-down" size={37} color="#4B6584" />
-        </TouchableOpacity>
-      </View>
+      {
+        moreInfo && (
+          <View style={styles.containerMoreInfo}>
+            <View style={styles.containerInps}>
+              <View style={styles.inputContainer}>
+                <Feather name="map-pin" size={24} color="#6B6B6B" style={styles.icon} />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Rua"
+                  value={address.street}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <MaterialIcons name="maps-home-work" size={19} color="#6B6B6B" style={styles.icon} />
+                <TextInput
+                  style={styles.input}
+                  placeholder="CEP"
+                  value={address.cep}
+                />
+              </View>
+              <View style={styles.containerRow}>
+                <View style={styles.inputContainer2}>
+                  <SimpleLineIcons name="map" size={19} color="#6B6B6B" style={styles.icon} />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Cidade"
+                    value={address.city}
+                  />
+                </View>
+                <View style={styles.inputContainer2}>
+                  <Feather name="globe" size={19} color="#6B6B6B" style={styles.icon} />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Estado"
+                    value={address.state}
+                  />
+                </View>
+              </View>
+              <View style={styles.inputContainer}>
+                <Feather name="grid" size={19} color="#6B6B6B" style={styles.icon} />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Complemento"
+                  value={address.complement}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <MaterialCommunityIcons name="keyboard" size={19} color="#6B6B6B" style={styles.icon} />
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  placeholder="Número"
+                  value={String(address.number)}
+                />
+              </View>
+            </View>
+          </View>
+        )
+      }
     </View>
   )
 }
--- src/components/CardAddress/styles.js
+++ src/components/CardAddress/styles.js
@@ -1,12 +1,16 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFFFFF",
+    marginVertical: 8,
+  },
   containerAddress: {
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 16,
     gap: 20,
-    paddingVertical: 14,
+    paddingVertical: 10,
     marginVertical: 10,
     backgroundColor: "#FFFFFF",
   },
@@ -33,6 +37,45 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: 'Poppins_600SemiBold',
     color: "#000",
+  },
+  containerMoreInfo: {
+    backgroundColor: "#fff",
+    borderColor: "#F5F5F5",
+    borderWidth: 2,
+    padding: 16,
+  },
+  containerInps: {
+    paddingHorizontal: 13,
+    paddingVertical: 10,
+  },
+  containerRow: {
+    flexDirection: "row",
+    gap: 15,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#F3F3F3",
+    borderRadius: 5,
+    padding: 16,
+    gap: 15,
+  },
+  inputContainer2: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#F3F3F3",
+    borderRadius: 5,
+    padding: 16,
+    gap: 15,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: "#6B6B6B",
+    fontFamily: 'Poppins_500Medium',
   },
 });
 
