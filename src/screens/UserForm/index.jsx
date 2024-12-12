d3f0261b7e44dee0c136e48280e79e2db7ccd231
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-12-12 17:23:42 -0300

    feat: update & fix keybord show

--- src/screens/UserForm/index.jsx
+++ src/screens/UserForm/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, TextInput, ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, ActivityIndicator, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
@@ -45,115 +45,117 @@ export default function UserForm({ route }) {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={{ alignItems: 'center' }}>
-        {msgPopUp ? <PopUp message={msgPopUp} /> : null}
-      </View>
-      <TouchableOpacity style={styles.containerIcon} onPress={() => navigation.goBack()}>
-        <AntDesign style={styles.icon} name="left" size={26} color="#000" />
-      </TouchableOpacity>
-      <View style={styles.containerTxt}>
-        <Text style={styles.txt}>Para concluir, precisamos de algumas informações para finalizar seu pedido</Text>
-      </View>
-      <View style={styles.containerInps}>
-        <View>
-          <View style={styles.containerInp}>
-            <Text style={styles.txtInp}>Nome</Text>
-            <TextInput onChangeText={setName} placeholder='José Carlos' style={styles.input} value={name} />
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={styles.container}>
+          <View style={{ alignItems: 'center' }}>
+            {msgPopUp ? <PopUp message={msgPopUp} /> : null}
           </View>
-          <View style={styles.containerInp}>
-            <Text style={styles.txtInp}>Telefone</Text>
-            <PhoneInput
-              value={inputValue}
-              placeholder='19 99999 9999'
-              onChangePhoneNumber={handleInputValue}
-              selectedCountry={selectedCountry}
-              onChangeSelectedCountry={handleSelectedCountry}
-              phoneInputStyles={{
-                container: {
-                  backgroundColor: '#f3f3f3',
-                  borderWidth: 0,
-
-                },
-                flagContainer: {
-                  borderTopLeftRadius: 12,
-                  borderBottomLeftRadius: 12,
-                  backgroundColor: '#f3f3f3',
-                  justifyContent: 'center',
-                },
-                flag: {},
-                caret: {
-                  color: '#F3F3F3',
-                  fontSize: 16,
-                },
-                divider: {
-                  backgroundColor: '#5E5E5E',
-                },
-                callingCode: {
-                  fontFamily: 'Poppins_400Regular',
-                  fontSize: 18,
-                  color: '#5E5E5E',
-                },
-                input: {
-                  color: '#5E5E5E',
-                  fontFamily: 'Poppins_400Regular',
-                  fontSize: 18,
-                },
-              }}
-              modalStyles={{
-                modal: {
-                  backgroundColor: '#fff',
-                },
-                backdrop: {},
-                divider: {
-                  backgroundColor: 'transparent',
-                },
-                countriesList: {},
-                searchInput: {
-                  borderRadius: 8,
-                  borderWidth: 0,
-                  borderColor: '#F3F3F3',
-                  color: '#5E5E5E',
-                  backgroundColor: '#F3F3F3',
-                  paddingHorizontal: 12,
-                  fontFamily: 'Poppins_400Regular',
-                  fontSize: 18,
-                  height: 46,
-                },
-                countryButton: {
-                  borderWidth: 0,
-                  borderColor: '#F3F3F3',
-                  backgroundColor: '#F3F3F3',
-                  marginVertical: 4,
-                  paddingVertical: 0,
-                },
-                noCountryText: {},
-                noCountryContainer: {},
-                flag: {
-                  color: '#FFFFFF',
-                  fontSize: 20,
-                },
-                callingCode: {
-                  color: '#5E5E5E',
-                  fontSize: 16,
-                  fontFamily: 'Poppins_400Regular',
-                },
-                countryName: {
-                  color: '#5E5E5E',
-                  fontSize: 16,
-                  fontFamily: 'Poppins_400Regular',
-                },
-              }}
-            />
+          <TouchableOpacity style={styles.containerIcon} onPress={() => navigation.goBack()}>
+            <AntDesign style={styles.icon} name="left" size={26} color="#000" />
+          </TouchableOpacity>
+          <View style={styles.containerTxt}>
+            <Text style={styles.txt}>Para concluir, precisamos de algumas informações para finalizar seu pedido</Text>
+          </View>
+          <View style={styles.containerInps}>
+            <View>
+              <View style={styles.containerInp}>
+                <Text style={styles.txtInp}>Nome</Text>
+                <TextInput onChangeText={setName} placeholder='José Carlos' style={styles.input} value={name} />
+              </View>
+              <View style={styles.containerInp}>
+                <Text style={styles.txtInp}>Telefone</Text>
+                <PhoneInput
+                  value={inputValue}
+                  placeholder='19 99999 9999'
+                  onChangePhoneNumber={handleInputValue}
+                  selectedCountry={selectedCountry}
+                  onChangeSelectedCountry={handleSelectedCountry}
+                  phoneInputStyles={{
+                    container: {
+                      backgroundColor: '#f3f3f3',
+                      borderWidth: 0,
+                    },
+                    flagContainer: {
+                      borderTopLeftRadius: 12,
+                      borderBottomLeftRadius: 12,
+                      backgroundColor: '#f3f3f3',
+                      justifyContent: 'center',
+                    },
+                    flag: {},
+                    caret: {
+                      color: '#F3F3F3',
+                      fontSize: 16,
+                    },
+                    divider: {
+                      backgroundColor: '#5E5E5E',
+                    },
+                    callingCode: {
+                      fontFamily: 'Poppins_400Regular',
+                      fontSize: 18,
+                      color: '#5E5E5E',
+                    },
+                    input: {
+                      color: '#5E5E5E',
+                      fontFamily: 'Poppins_400Regular',
+                      fontSize: 18,
+                    },
+                  }}
+                  modalStyles={{
+                    modal: {
+                      backgroundColor: '#fff',
+                    },
+                    backdrop: {},
+                    divider: {
+                      backgroundColor: 'transparent',
+                    },
+                    countriesList: {},
+                    searchInput: {
+                      borderRadius: 8,
+                      borderWidth: 0,
+                      borderColor: '#F3F3F3',
+                      color: '#5E5E5E',
+                      backgroundColor: '#F3F3F3',
+                      paddingHorizontal: 12,
+                      fontFamily: 'Poppins_400Regular',
+                      fontSize: 18,
+                      height: 46,
+                    },
+                    countryButton: {
+                      borderWidth: 0,
+                      borderColor: '#F3F3F3',
+                      backgroundColor: '#F3F3F3',
+                      marginVertical: 4,
+                      paddingVertical: 0,
+                    },
+                    noCountryText: {},
+                    noCountryContainer: {},
+                    flag: {
+                      color: '#FFFFFF',
+                      fontSize: 20,
+                    },
+                    callingCode: {
+                      color: '#5E5E5E',
+                      fontSize: 16,
+                      fontFamily: 'Poppins_400Regular',
+                    },
+                    countryName: {
+                      color: '#5E5E5E',
+                      fontSize: 16,
+                      fontFamily: 'Poppins_400Regular',
+                    },
+                  }}
+                />
+              </View>
+            </View>
+            <TouchableOpacity onPress={handleSubmit} style={styles.btn}>
+              {
+                globalLoading ? <ActivityIndicator size="large" color={"#fff"} /> : <Text style={styles.txtBtn}>Continuar</Text>
+              }
+            </TouchableOpacity>
           </View>
         </View>
-        <TouchableOpacity onPress={handleSubmit} style={styles.btn}>
-          {
-            globalLoading ? <ActivityIndicator size="large" color={"#fff"} /> : <Text style={styles.txtBtn}>Continuar</Text>
-          }
-
-        </TouchableOpacity>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
